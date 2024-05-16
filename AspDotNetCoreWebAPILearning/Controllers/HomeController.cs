05aa6b0681e02300244a402d59e7e0444278dacc
sandeep kumar <sandeepkumar.sk5@outlook.com>
2024-05-17 00:16:20 +0530

    Add project files.

---
+++ AspDotNetCoreWebAPILearning/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspDotNetCoreWebAPILearning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GetAppSettingKeyValue()
+        {
+            return Ok(_configuration["FullName"]);
+        }
+    }
+}
